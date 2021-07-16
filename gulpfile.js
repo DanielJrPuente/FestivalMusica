eac7485a8b717e53ce889e86dd22bba58b2a4055
DanielJrPuente <danpmpj@gmail.com>
2021-07-15 17:47:19 -0700

    Se agregaron las funciones de minificar imagenes y de convertirlas a formato webp y de incluir notificaciones

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
-const { series, src, dest, watch} = require('gulp');
-
+const { series, src, dest, watch, parallel} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
+
 
 // Funsion que compila sass
 
@@ -12,7 +15,10 @@ function css(){
     .pipe(dest("./build/css"))
 }
 
+
+
 // funcion de minificar css
+
 function minificarCSS(){
   return src("./src/scss/app.scss")
     .pipe(sass({
@@ -21,6 +27,24 @@ function minificarCSS(){
     .pipe(dest("./build/css"))
 }
 
+//Minificar imagenes
+function imagenes() {
+  return src("./src/img/**/*")
+    .pipe( imagemin())
+    .pipe(dest('./build/img'))
+    .pipe(notify({message: 'Imagen minificada'}));
+}
+
+// Imagenes webp
+
+function vercionWebp () {
+  return src("./src/img/**/*")
+    .pipe( webp() )
+    .pipe(dest("./build/img"))
+    .pipe(notify({message: 'Vercion webp'}));
+}
+
+
 
 function watchArchivos() {
   watch("./src/scss/**/*.scss", css); // * = a la carpeta actual -- ** = a todos los archivos con esa extencion
@@ -28,5 +52,7 @@ function watchArchivos() {
 
 exports.css = css;
 exports.minificarCSS = minificarCSS;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
 
+exports.default = series( css, imagenes, vercionWebp, watchArchivos);
